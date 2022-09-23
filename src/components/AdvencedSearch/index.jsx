6a707469dc95c7ef56db99ddb936e951cfa542e6
anonymousvegan <majjunjulavgust@gmail.com>
2022-09-23 16:18:01 +0200

    add checkbox filters json

--- src/components/AdvencedSearch/index.jsx
+++ src/components/AdvencedSearch/index.jsx
@@ -1,8 +1,28 @@
 import Layout from "../Layout";
 import style from "./style.module.css";
-
+import { useState } from "react";
+import filters from "fiters.json";
 const Advencedsearch = () => {
-    return <Layout>Advencedsearch</Layout>;
+    const [title, setTitle] = useState("");
+
+    return (
+        <Layout>
+            <section className={style.filters}>
+                <div>
+                    <h3>Title</h3>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        placeholder="Enter title"
+                    />
+                </div>
+                <div>
+                    <h3>Title type</h3>
+                </div>
+            </section>
+        </Layout>
+    );
 };
 
 export default Advencedsearch;
